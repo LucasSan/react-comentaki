7972e7e719271c696f44bbab909992b9cfe833a7
Lucas Germano <lucas.germano@farfetch.com>
2020-02-24 16:55:53 +0000

    master

--- src/App.js
+++ src/App.js
@@ -1,21 +1,76 @@
 import React, { useState, useEffect } from 'react'
 import firebase from './firebase'
 
-const Comments = () =>
-
-function App() {
+const useDatabase = endpoint => {
   const [data, setData] = useState({})
   
   useEffect(() => {
-    const ref = firebase.database().ref('teste')
+    const ref = firebase.database().ref(endpoint)
     ref.on('value', snapshot => {
-      console.log(snapshot.val())
       setData(snapshot.val())
     })
-  }, [])
+
+    return () => {
+      ref.off()
+    }
+  }, [endpoint])
+
+  return data
+}
+
+const useDatabasePush = endpoint => {
+  const [status, setStatus] = useState('')
+  const save = data => {
+    const ref = firebase.database().ref(endpoint)
+    ref.push(data, err => {
+      if (err) {
+        setStatus('ERROR')
+      } else {
+        setStatus('SUCCESS')
+      }
+    })
+  }
+
+  return [status, save]
+}
+
+const Comment = ({ comment }) => {
+  return (
+    <div>
+      {comment.content} por: { comment.user.name }
+    </div>
+  )
+}
+
+const Comments = () => {
+  const data = useDatabase('comments')
+  if (!data) {
+    return <p>Nenhum comentario enviado ate o momento</p>
+  }
+
+  const ids = Object.keys(data)
+
+  if (ids.length === 0) {
+    return <p>Carregando...</p>
+  }
+
+  return ids.map(id => {
+    return <Comment key={id} comment={data[id]} />
+  })
+}
+
+function App() {
+  const [, save] = useDatabasePush('comments')
 
   return <div>
-    {JSON.stringify(data)}
+    <button onClick={() => {
+      save({ content: 'ola aqui e meu comentario',
+        user: {
+          id: '1',
+          name: 'Lucas'
+        } })
+    }}>Toggle</button>
+    <Comments />
   </div>
 }
 
